f4573b211b81c74f23fd24507fb70cb0339431f1
shadzijusu <hadzijusufovicselma@gmail.com>
2022-10-02 17:31:49 +0200

    temp change

--- src/app/core/services/CartService.service.ts
+++ src/app/core/services/CartService.service.ts
@@ -8,6 +8,7 @@ import { Cart } from '../models/Cart.model';
 @Injectable({providedIn: 'root'})
 export class CartService {
     constructor(private http: HttpClient) {}
+    
     getCarts(): Observable<Cart[]> {
         return this.http.get<Cart[]>(`https://fakestoreapi.com/carts`);
     }
